e300033254d09cdcd9949c00f77f9f9010c6bb46
p3zhy <pejman.ghd@gmail.com>
2024-03-07 10:44:46 +0330

    Example edited

--- examples/esp32x.rs
+++ examples/esp32x.rs
@@ -1,26 +1,18 @@
 use accelerometer::{RawAccelerometer, Tracker};
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::i2c::*;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::prelude::*;
-use esp_idf_sys;
-use kxtj3_1057::{Kxtj3, Mode, Range, SlaveAddr};
+use esp_idf_svc::hal::{delay::FreeRtos, i2c::*, prelude::Peripherals, units::Hertz};
+use kxtj3_1057::{register::SlaveAddr, Kxtj3};
 fn main() {
-    esp_idf_sys::link_patches();
-
+    esp_idf_svc::sys::link_patches();
     let peripherals = Peripherals::take().unwrap();
     let i2c = peripherals.i2c0;
     let sda = peripherals.pins.gpio10;
     let scl = peripherals.pins.gpio8;
-
     let config = I2cConfig::new()
-        .baudrate(400.kHz().into())
+        .baudrate(Hertz(400_000))
         .scl_enable_pullup(true)
         .sda_enable_pullup(true);
     let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
-
     let mut kxtj3 = Kxtj3::new(i2c, SlaveAddr::Default).unwrap();
-
     let mut tracker = Tracker::new(3700.0);
     loop {
         let accel = kxtj3.accel_raw().unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -58,32 +58,24 @@ pub struct Kxtj3<I2C> {
     address: u8,
 }
 
-// impl<I2C, E> Kxtj3<I2C>
-// where
-//     I2C: WriteRead<Error = E> + i2c::Write<Error = E>,
-
 impl<I2C, E> Kxtj3<I2C>
 where
     I2C: I2c<Error = E>,
 {
     /// Create a new KXTJ3-1057 driver from the given I2C peripheral.
-    /// Default is Hz_400 LowPower.
+    /// Default is Hz_400.
     /// An example using the [esp_idf_hal](https://esp-rs.github.io/esp-idf-hal/esp_idf_hal):
     ///     
-    ///     use esp_idf_hal::i2c::*;
-    ///     use esp_idf_hal::peripherals::Peripherals;
-    ///     use esp_idf_hal::prelude::*;
-    ///     use kxtj3_1057::Kxtj3;
+    ///     use esp_idf_svc::hal::{delay::FreeRtos, i2c::*, prelude::Peripherals, units::Hertz};
+    ///     use kxtj3_1057::{register::SlaveAddr, Kxtj3};
     ///           
     ///     let peripherals = Peripherals::take().unwrap();
     ///     let i2c = peripherals.i2c0;
-    ///     
     ///     let sda = peripherals.pins.gpio10;
     ///     let scl = peripherals.pins.gpio8;
-    ///     let config = I2cConfig::new().baudrate(400.kHz().into()).scl_enable_pullup(true).sda_enable_pullup(true);
-    ///
-    ///     let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();     
-    ///     let kxtj3 = Kxtj3::new(i2c, kxtj3_1057::SlaveAddr::Default).unwrap();     
+    ///     let config = I2cConfig::new().baudrate(Hertz(400_000)).scl_enable_pullup(true).sda_enable_pullup(true);
+    ///     let i2c = I2cDriver::new(i2c, sda, scl, &config).unwrap();
+    ///     let mut kxtj3 = Kxtj3::new(i2c, SlaveAddr::Default).unwrap();
 
     pub fn new(i2c: I2C, address: SlaveAddr) -> Result<Self, Error<E, core::convert::Infallible>> {
         Self::new_with_config(i2c, address, Configuration::default())
